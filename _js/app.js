5eb138c0d8849736dff91169a749409b32d5188a
Filipe Silva <filipesilva.dev@gmail.com>
2016-08-18 15:41:57 -0300

    starting animations w/ animate.css

--- _js/app.js
+++ _js/app.js
@@ -1,7 +1,27 @@
 $( function() {
-	$( "#tabs" ).tabs();
 
-	var formLogin = $('form-login');
-	console.log(formLogin);
+	//House and Buffets tab
+	$('#tabs').tabs();
+
+	//Animations
+	var logo   = $('.hp-head .efestas-logo'),
+		slogan = $('.hp-head .slogan'),
+		search = $('.hp-search');
+
+		//Entrances
+		logo.addClass('animated slideInDown');
+		slogan.addClass('animated rollIn');
+		search.addClass('animated slideInUp');
+
+		//Hover action	
+		logo.hover(function() {
+			if (!logo.hasClass('pulse')) {
+				logo.removeClass('slideInDown');
+				logo.addClass('pulse');
+			};
+		},function() {
+			logo.removeClass('pulse');
+		});
+
 });
 
